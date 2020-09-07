7e90e1bb76c353194ba54b5977cc3dcb91ed2ea8
andrea <chiabotto@newsaktuell.de>
2020-09-07 21:23:36 +0200

    select component / header structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import "./App.scss";
 
 function App() {
+  const [countries, setCountries] = useState(["A", "B", "C"]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    const getCountriesByData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const fetchedCountries = data.map((fetchedCountry) => ({
+            name: fetchedCountry.country,
+            value: fetchedCountry.countryInfo.iso2,
+          }));
+
+          setCountries(fetchedCountries);
+        });
+    };
+
+    getCountriesByData();
+  }, []);
+
+  const onCOuntryChange = async (event) => {
+    const selectedCountry = event.target.value;
+
+    setCountry(selectedCountry)
+  };
+
   return (
     <div className="app">
-      <h1>test</h1>
+      <div className="app__header">
+        <h1>Covid-19 World Tracker</h1>
+
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCOuntryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem
+                key={"country item " + country.value}
+                value={country.value}
+              >
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      <div className="app__stats"></div>
+
+      {/* Infobox */}
+      {/* Infobox */}
+      {/* Infobox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
