11ea41879e59ccde3550b0848a51ec872be445ce
andrea <chiabotto@newsaktuell.de>
2020-09-09 23:37:15 +0200

    removing css classes

--- src/components/InfoBox/infoBox.js
+++ src/components/InfoBox/infoBox.js
@@ -5,11 +5,9 @@ function infoBox({ title, cases, total }) {
   return (
     <Card className="infoBox">
       <CardContent>
-        
-        <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
-        <h2 className="infoBox__cases">{cases}</h2>
-        <Typography className="infoBox__total"  color="textSecondary">{total} Total</Typography>
-
+        <Typography color="textSecondary">{title}</Typography>
+        <h2>{cases}</h2>
+        <Typography color="textSecondary">{total} Total</Typography>
       </CardContent>
     </Card>
   );
