1e653d954abb7335a3f168d1d7697606c24529df
andrea <chiabotto@newsaktuell.de>
2020-09-27 19:49:59 +0200

    popup and numeral utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -23,20 +23,39 @@ export const sortData = (data) => {
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
 };
 
-export const showDataOnMap = (data, casesType = "cases") => {
+export const prettyPrintStat = (stat) =>
+  stat ? `${numeral(stat).format("0.0a")}` : "0";
+
+export const showDataOnMap = (data, casesType = "cases") =>
   data.map((country) => (
     <Circle
       center={[country.countryInfo.lat, country.countryInfo.long]}
-      fillOpacity={0.3}
       color={casesTypeColors[casesType].hex}
       fillColor={casesTypeColors[casesType].hex}
+      fillOpacity={0.4}
       radius={
         Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
       }
     >
       <Popup>
-        <h1>popup</h1>
+        <div className="country-popup">
+          <div
+            className="country-popup__flag"
+          >
+            <img src={`${country.countryInfo.flag}`} alt={`${country.country} Flag`}/>
+          </div>
+          <h2 className="country-popup__name">{country.country}</h2>
+          <p className="country-popup__confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </p>
+          <p className="country-popup__recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </p>
+          <p className="country-popup__deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </p>
+        </div>
       </Popup>
     </Circle>
   ));
-};
+
