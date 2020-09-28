0b1f45345b768c4fefb1f716ac0674db9bbbd755
andrea <chiabotto@newsaktuell.de>
2020-09-28 22:00:49 +0200

    moving leaflet css into the right component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 import {showDataOnMap} from "../../utils/utils"
 
 import "./Map.scss";
@@ -8,18 +10,19 @@ import "./Map.scss";
 
 function Map({countries, casesType, center, zoom}) {
 
-  
 
   return (
     <div className="Map">
       <LeafletMap center={center} zoom={zoom}>
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
        
-        {showDataOnMap(countries, casesType)}
-       
+       {/*
+      {showDataOnMap(countries, casesType)}
+      */}
+
       </LeafletMap>
     </div>
   );
