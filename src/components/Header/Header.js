caa54540e867678ed095b5df6b6f72b24bf61e3e
andrea <chiabotto@newsaktuell.de>
2020-10-10 21:00:47 +0200

    Improving css flexbox for the Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,27 +5,29 @@ import "./Header.scss";
 
 const Header = (props) => {
   return (
-    <Grid container spacing={3} className="Header">
-      <Grid item xs={12}>
-        <h1>Covid-19 Daily Tracker</h1>
-      </Grid>
-
-      <Grid item xs={12}>
-        <h3>Select a country: </h3>
-        <FormControl>
-          <Select
-            variant="outlined"
-            onChange={props.onCountryChange}
-            value={props.country}
-          >
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {props.countries.map((country, i) => (
-              <MenuItem key={"country nr " + i} value={country.value}>
-                {country.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+    <Grid container spacing={3} >
+      <Grid item xs={12} className="Header">
+        <div className="Header__title">
+          <h1>Covid-19 Daily Tracker</h1>
+        </div>
+       
+        <div className="Header__select">
+          <h3>Select a country: </h3>
+          <FormControl>
+            <Select
+              variant="outlined"
+              onChange={props.onCountryChange}
+              value={props.country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {props.countries.map((country, i) => (
+                <MenuItem key={"country nr " + i} value={country.value}>
+                  {country.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
       </Grid>
     </Grid>
   );
