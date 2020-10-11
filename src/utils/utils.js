c7713c70d9faa347481d58493b431fa6f88d6572
andrea <chiabotto@newsaktuell.de>
2020-10-11 08:13:36 +0200

    utils clean up

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,38 +1,4 @@
-//import React from "react";
 import numeral from "numeral";
-//import { Circle, Popup } from "react-leaflet";
-
-const casesTypeColors = {
-  cases: {
-    hex: "#cc1034",
-    multiplier: 800,
-  },
-  recovered: {
-    hex: "#7dd71d",
-    multiplier: 800,
-  },
-  deaths: {
-    hex: "#fb4443",
-    multiplier: 2000,
-  },
-};
-
-// load a language
-numeral.register('locale', 'de', {
-  delimiters: {
-      thousands: '.',
-      decimal: ','
-  },
-  abbreviations: {
-      thousand: 'k',
-      million: 'm',
-      billion: 'b',
-      trillion: 't'
-  }
-});
-
-// switch between languages
-// numeral.locale('de');
 
 export const sortData = (data,type="cases",max=10) => {
   const sortedData = [...data];
