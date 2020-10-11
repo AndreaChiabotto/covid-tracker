e92ed7486a54ed2b2fd9446d6364f66c0c6ec4c0
andrea <chiabotto@newsaktuell.de>
2020-10-11 07:29:31 +0200

    code clean up

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
-import React from "react";
+//import React from "react";
 import numeral from "numeral";
-import { Circle, Popup } from "react-leaflet";
+//import { Circle, Popup } from "react-leaflet";
 
 const casesTypeColors = {
   cases: {
@@ -46,37 +46,3 @@ export const prettyPrintStat = (stat) => {
   }
 };
 
-export const showDataOnMap = (data, casesType = "cases") =>
-  data.map((country) => (
-    <Circle
-      key={country.country + "_" + casesType}
-      center={[country.countryInfo.lat, country.countryInfo.long]}
-      color={casesTypeColors[casesType].hex}
-      fillColor={casesTypeColors[casesType].hex}
-      fillOpacity={0.4}
-      radius={
-        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
-      }
-    >
-      <Popup>
-        <div className="country-popup">
-          <div className="country-popup__flag">
-            <img
-              src={`${country.countryInfo.flag}`}
-              alt={`${country.country} Flag`}
-            />
-          </div>
-          <h2 className="country-popup__name">{country.country}</h2>
-          <p className="country-popup__confirmed">
-            Cases: {numeral(country.cases).format("0,0")}
-          </p>
-          <p className="country-popup__recovered">
-            Recovered: {numeral(country.recovered).format("0,0")}
-          </p>
-          <p className="country-popup__deaths">
-            Deaths: {numeral(country.deaths).format("0,0")}
-          </p>
-        </div>
-      </Popup>
-    </Circle>
-  ));
